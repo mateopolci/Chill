f335751d09175aa312d8a0e305773b713088516b
mateopolci <mateopolci@hotmail.com>
2024-09-01 01:20:38 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 function App() {
 	return (
 		<>
-
+			<NavBar />
 		</>
 	);
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import chill from "../../assets/chill.png";
+function NavBar() {
+	return (
+		<header className="flex flex-row justify-between bg-black">
+			<a href="#" className="w-32">
+				<img src={chill} alt="Chill logo" />
+			</a>
+			<div className="flex">
+				<ul className="flex flex-row justify-center items-center">
+					<li className="text-light-green text-2xl font-bold">
+						<a href="#" className="ml-10 mr-5 p-3">
+							Shop
+						</a>
+					</li>
+					<li className="text-light-green text-2xl font-bold">
+						<a href="#" className="mx-5 p-3">
+							About us
+						</a>
+					</li>
+					<li className="text-light-green text-2xl font-bold">
+						<a href="#" className="ml-5 mr-10 p-3">
+							Contact
+						</a>
+					</li>
+				</ul>
+				<></>
+			</div>
+		</header>
+	);
+}
+
+export default NavBar;
